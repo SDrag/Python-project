57624492608444832c894f7b4c100464a21607e7
Dragutin Sreckovic <35846309+SDrag@users.noreply.github.com>
2018-04-28 10:48:27 +0100

    Update irisProj.py
    playing around with pandas and dataset to see what I can do

--- irisProj.py
+++ irisProj.py
@@ -2,3 +2,23 @@
 
 # Python project on iris dataset
 
+
+import pandas as pnd
+import matplotlib.pyplot as mpl
+
+dataFile = 'iris.csv'
+dataSet = pnd.read_csv(dataFile)
+
+for iris in dataSet['class'].unique():
+    print(iris)
+
+print(dataSet.groupby('class').size())
+
+print(dataSet.describe())
+
+irs = {}
+for iris in dataSet['class'].unique():
+    irs[iris] = dataSet[dataSet['class']==iris]
+    print(irs[iris].describe())
+    
+ 
